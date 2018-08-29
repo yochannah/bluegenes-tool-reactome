71a48cacbd5f80be7f6c67693128e635724f09c8
Yo Yehudi <yochannah@gmail.com>
2018-08-29 13:57:13 +0100

    some fool committed node_modules

--- src/pathwayQuery.js
+++ src/pathwayQuery.js
@@ -1,20 +1,26 @@
-export default pathwayQuery = {
-  "from": "Pathway",
-  "select": [
-    "identifier",
-    "name",
-    "dataSets.name"
-  ],
-  "orderBy": [
-    {
+/*  Builds a query for pathways since lookup by ID doesn't return the datasets
+    field, which we need in order to exclude non-reactome datasets. You can't
+    really blame Reactome for not understanding Kegg pathway ids. */
+export default function pathwayQuery(val) {
+  return {
+    "from": "Pathway",
+    "select": [
+      "identifier",
+      "name",
+      "dataSets.name"
+    ],
+    "orderBy": [{
       "path": "identifier",
       "direction": "ASC"
-    }
-  ],
-  "where": [
-  {
-    "path": "id",
-    "op": "="
-  }
-]
+    }],
+    "where": [{
+      "path": "id",
+      "op": "=",
+      "value": val
+    }, {
+      "path": "dataSets.name",
+      "op": "==",
+      "value": "Reactome pathways data set"
+    }]
+  };
 };
